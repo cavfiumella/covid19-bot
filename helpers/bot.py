cavfiumella/covid19-bot

commit eb6a52912bc4992a4a6cd01ff5eddee7c513ca56

do not catch unexpected errors in previous settings restoration

helpers/bot.py
@@ -298,7 +298,7 @@ class MyBot:
             reply_markup=ReplyKeyboardRemove()
         )
 
-        try:
+        if "previous_report_settings" in cotext.chat_data:
             # restore previous configuration
             previous = context.chat_data["previous_report_settings"].copy()
             context.chat_data.clear()
@@ -308,7 +308,7 @@ class MyBot:
                 f"Previous settings restored: {context.chat_data}"
             )
 
-        except:
+        else:
             # clear current configuration
             context.chat_data.clear()
 

commit 304443d1b13037f53bb615543f1fea623845aa9f

fix typo introduced in commit eb6a52912bc4992a4a6cd01ff5eddee7c513ca56

helpers/bot.py
@@ -298,7 +298,7 @@ class MyBot:
             reply_markup=ReplyKeyboardRemove()
         )
 
-        if "previous_report_settings" in cotext.chat_data:
+        if "previous_report_settings" in context.chat_data:
             # restore previous configuration
             previous = context.chat_data["previous_report_settings"].copy()
             context.chat_data.clear()