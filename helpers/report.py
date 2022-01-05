cavfiumella/covid19-bot

commit a2d02be367b3038ccc098153e29bc0260f86fbf9

Minimal and clever `/attiva_report` conversation
Previously `/attiva_report` had the following states:
1. set reports *format*,
2. set reports delivery *frequency*,
3. set *contagions national* reports,
4. set *contagions regional* reports,
5. set *vaccines national* reports,
6. set *vaccines regional* reports.

Now conversation has less states:
1. set reports *format*,
2. set reports *reference periods*,
3. set *contagions* reports *areas*,
3. set *vaccines* reports *areas*.

In addition, multiple anserws were accepted for regional settings and
they were read from multiple messages getting one option each time.
This was effective but not efficient.

New changes make the bot able to detect multiple options in a single
message, making the conversation smoother and more intuitive.

Changes done here could led to incompatibility with previous versions
but for now is left code to make them compatible.
For a future major release that will be dropped.

helpers/report.py
@@ -148,19 +148,34 @@ class Reporter(Scheduler):
     _tz: str = "Europe/Rome"
     _do_not_disturb: Tuple = ("21:00", "10:00")
 
-    # report delivery frequencies
-    _frequencies: list[str] = ["Giornaliera", "Settimanale", "Mensile"]
+    # report delivery periods
+    _periods: list[str] = [
+        "giorno", "settimana", "mese",
+
+        # backward compatibility
+        "Giornaliera", "Settimanale", "Mensile"
+    ]
 
     # correspondig datetime fmt for frquencies
-    _frequency_fmt: Dict[str, str] = {
+    _period_fmt: Dict[str, str] = {
+        "giorno": "%Y-%m-%d",
+        "settimana": "anno %Y, settimana %W",
+        "mese": "%Y-%m",
+
+        # backward compatibility
         "Giornaliera": "%Y-%m-%d",
         "Settimanale": "anno %Y, settimana %W",
         "Mensile": "%Y-%m"
     }
 
     # offsets used to determine current period in report generation;
-    # values are (frequency, days offset) pairs
-    _frequency_offset: Dict[str, int] = {
+    # values are (period, days offset) pairs
+    _period_offset: Dict[str, int] = {
+        "giorno": 0,
+        "settimana": -7,
+        "mese": -30,
+
+        # backward compatibility
         "Giornaliera": 0,
         "Settimanale": -7,
         "Mensile": -30
@@ -373,7 +388,7 @@ class Reporter(Scheduler):
         # generate reports
         reports = []
 
-        if settings.get(f"{db_key}_national") == "Sì":
+        if settings.get(db_key) != None and "Italia" in settings.get(db_key):
             report = self.get_report(
                 self._db[db_key].get_df(self._db_files_keys[db_key]["national"]),
                 variables = self._db_variables[db_key], current = current,
@@ -385,10 +400,16 @@ class Reporter(Scheduler):
 
             reports += [report]
 
-        regions = settings.get(f"{db_key}_regional")
+        regions = settings.get(db_key)
 
         if type(regions) == str:
             regions = [regions]
+
+        if type(regions) == list:
+            try:
+                regions.pop("Italia")
+            except:
+                pass
 
         if regions != None:
             for region in regions:
@@ -410,7 +431,7 @@ class Reporter(Scheduler):
         # format and send reports
 
         # textual format
-        if settings.get("format") == "Testuale":
+        if settings.get("format") == "testuale":
 
             self._bot.get_chat_logger(chat_id).debug("Sending textual report")
 
@@ -517,23 +538,23 @@ class Reporter(Scheduler):
                 db.update()
 
             for chat_id, settings in self._bot.get_chat_data().items():
-                for frequency in self._frequencies:
+                for period in self._periods:
 
-                    fmt = self._frequency_fmt[frequency]
+                    fmt = self._period_fmt[period]
 
                     self._bot.get_chat_logger(chat_id).debug(
                         f"Settings: {settings}"
                     )
 
                     current = now \
-                    + pd.Timedelta(days=self._frequency_offset[frequency])
+                    + pd.Timedelta(days=self._period_offset[period])
                     current = current.strftime(fmt)
 
                     # skip user
-                    if settings.get("frequency") != frequency:
+                    if settings.get("period") != period:
                         self._bot.get_chat_logger(chat_id).debug(
-                            "Skipping report delivery with frequency "
-                            f"\"{frequency}\": not subscribed"
+                            "Skipping report delivery with period "
+                            f"\"{period}\": not subscribed"
                         )
                         continue
 
@@ -543,8 +564,8 @@ class Reporter(Scheduler):
                         if type(settings.get("last_report")) == dict \
                         and current == settings["last_report"].get(db_key):
                             self._bot.get_chat_logger(chat_id).debug(
-                                "Skipping report delivery with frequency "
-                                f"\"{frequency}\": already sent"
+                                "Skipping report delivery with period "
+                                f"\"{period}\": already sent"
                             )
                             continue
 

helpers/bot.py
@@ -11,9 +11,7 @@ from pathlib import Path
 from telegram import (
     Update,
     Bot,
-    Chat,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove
+    Chat
 )
 from telegram.ext import (
     Updater,
@@ -73,14 +71,29 @@ class MyBot:
         "versione": "visualizza versione bot"
     }
 
-    # reports settings; values are (setting, available answers) pairs
-    _report_settings: Dict[str, List[str]] = {
-        "format": ["Testuale", "Excel"],
-        "frequency": ["Giornaliera", "Settimanale", "Mensile"],
-        "contagions_national": ["Sì", "No"],
-        "contagions_regional": None,
-        "vaccines_national": ["Sì", "No"],
-        "vaccines_regional": None
+    # reports delivery settings; values are (setting, available options) pairs
+    _settings: Dict[str, List[str]] = {
+        "format": ["testuale", "excel"], # report format
+        "period": ["giorno", "settimana", "mese"], # reference time period
+        "contagions": ["Italia"], # selected areas (Italy or regions)
+        "vaccines": ["Italia"] # selected areas (Italy or regions)
+        # regions are appended later
+    }
+
+    # mandatory settings
+    _mandatory: Dict[str,bool] = {
+        "format": True,
+        "period": True,
+        "contagions": False,
+        "vaccines": False
+    }
+
+    # settings with multiple answers allowed
+    _multiples: Dict[str,bool] = {
+        "format": False,
+        "period": False,
+        "contagions": True,
+        "vaccines": True
     }
 
 
@@ -279,53 +292,47 @@ class MyBot:
         # store answer to previous question
         else:
 
-            # not a regional setting
-            if setting == None or "regional" not in setting:
-                context.chat_data.update({setting: update.message.text})
+            # parse answer
+            text = update.message.text.lower()
+            answer = []
 
-            # regional setting
-            else:
+            for option in self._settings[setting]:
+                if option.lower() in text:
+                    answer += [option]
 
-                # append to list or create a new one with regions
-                if setting in context.chat_data:
-                    # avoid inserting duplicates
-                    if update.message.text not in context.chat_data[setting]:
-                        context.chat_data[setting] += [update.message.text]
-                else:
-                    context.chat_data.update({setting: [update.message.text]})
+            # invalid answer
+            if self._mandatory[setting] and len(answer) == 0:
+                return self._cancel_conversation(
+                    update=update, context=context, invalid_answer=True
+                )
+
+            # if multiple answers are not allowed take just the first valid
+            if not self._multiples[setting]:
+                answer = answer[0]
+
+            context.chat_data.update({setting: answer})
 
             self.get_chat_logger(chat.id).debug(
                 f"Setting: \"{setting}\" = \"{context.chat_data.get(setting)}\""
             )
 
-        settings = list(self._report_settings.keys())
+        settings = list(self._settings.keys())
 
         # ask first question
         if setting == None:
             setting = settings[0]
 
-        # keep asking for more regions
-        elif "regional" in setting and update.message.text != "Nessun report":
-            pass
-
-        # go to the next question
+        # ask next question
         elif settings.index(setting) + 1 < len(settings):
             setting = settings[settings.index(setting)+1]
 
         # conversation is over: time for words is over!
         else:
-
-            # remove unneeded "Nessun report" values in multiple regions lists
-            for key in context.chat_data.keys():
-                if "regional" in key and len(context.chat_data[key]) > 1:
-                    context.chat_data[key].remove("Nessun report")
-
             self.get_chat_logger(chat.id).info(
                 f"Report settings: {context.chat_data}"
             )
             self.send_message(
                 chat.id, path=self._msg_dir.joinpath("setting_end.md"),
-                reply_markup=ReplyKeyboardRemove()
             )
 
             # show current settings
@@ -336,10 +343,25 @@ class MyBot:
         # ask question
         self.send_message(
             chat.id, path=self._msg_dir.joinpath(f"{setting}_setting.md"),
-            reply_markup=ReplyKeyboardMarkup(
-                np.array(self._report_settings[setting]).reshape(-1,1),
-                one_time_keyboard=True
-            )
+        )
+
+        return setting
+
+
+    def _show_options(
+        self, update: Update, context: CallbackContext, setting: str
+    ) -> str:
+        """/opzioni command.
+        Show available options while in conversation with `/attiva_report`.
+        """
+
+        chat_id = update.effective_chat.id
+
+        self.get_chat_logger(chat_id).debug("/opzioni command")
+
+        self.send_message(
+            chat_id, path=self._msg_dir.joinpath("options.md"),
+            fmt = ("\- *" + "*\n\- *".join(self._settings[setting]) + "*",)
         )
 
         return setting
@@ -373,14 +395,13 @@ class MyBot:
         )
 
         self.send_message(
-            chat.id, path=self._msg_dir.joinpath("cancel_setting.md"),
-            reply_markup=ReplyKeyboardRemove()
+            chat.id, path=self._msg_dir.joinpath("cancel_setting.md")
         )
 
         if invalid_answer:
             self.send_message(
                 chat.id, path=self._msg_dir.joinpath("invalid_answer.md"),
-                fmt=(update.message.text,), reply_markup=ReplyKeyboardRemove()
+                fmt=(update.message.text,)
             )
 
         # show current settings
@@ -432,10 +453,7 @@ class MyBot:
         # build send_message fmt argument
         fmt = [
             settings.get(key)
-            for key in [
-                "frequency", "contagions_national", "contagions_regional",
-                "vaccines_national", "vaccines_regional"
-            ]
+            for key in ["format", "period", "contagions", "vaccines"]
         ]
 
         # convert list to a nice string for printing
@@ -538,12 +556,10 @@ class MyBot:
             regions = regions.loc[:, area_column].drop_duplicates()
             regions = regions.sort_values().tolist()
 
-            self._report_settings[f"{key}_regional"] = ["Nessun report"]
-            self._report_settings[f"{key}_regional"] += regions
+            self._settings[key] = ["Italia"] + regions
 
             self._logger.debug(
-                f"Available {key} regions: "
-                f"{self._report_settings[f'{key}_regional']}"
+                f"Available {key} regions: {self._settings[key]}"
             )
 
 
@@ -690,15 +706,20 @@ class MyBot:
         self._dispatcher.add_handler(ConversationHandler(
             entry_points=[CommandHandler("attiva_report", self._enable_reports)],
             states = {
-                setting: [MessageHandler(
-                    Filters.update.message \
-                    & Filters.text(self._report_settings[setting]),
-                    partial(self._enable_reports, setting=setting)
-                )]
-                for setting in self._report_settings.keys()
+                setting: [
+                    CommandHandler("annulla", self._cancel_conversation),
+                    CommandHandler(
+                        "opzioni", partial(self._show_options, setting=setting)
+                    ),
+                    MessageHandler(
+                        Filters.update.message & Filters.text,
+                        partial(self._enable_reports, setting=setting)
+                    )
+                ]
+                for setting in self._settings.keys()
             },
             fallbacks = [
-                CommandHandler("annulla", self._cancel_conversation),
+                #CommandHandler("annulla", self._cancel_conversation),
                 MessageHandler(
                     ~ Filters.update.edited_message,
                     partial(self._cancel_conversation, invalid_answer=True)

commit 4c0c4c5151424d07304fa96d7649395def7ca098

Fix bug Italy considered as region
Commit a2d02be367b3038ccc098153e29bc0260f86fbf9 introduced as error
when Reporter removes "Italia" from the list of regions.
`list.pop` fails because it is misused, "Italia" remains in regions list
and it generates a fatal error while sending reports.

This commit fixes the bug replacing `list.pop` with `list.remove` and
replacing `except` with `except ValueError`; in this way if there is an
unexpected error it is not ignored as before.

helpers/report.py
@@ -392,8 +392,8 @@ class Reporter(Scheduler):
 
         if type(regions) == list:
             try:
-                regions.pop("Italia")
-            except:
+                regions.remove("Italia")
+            except ValueError:
                 pass
 
         if regions != None: